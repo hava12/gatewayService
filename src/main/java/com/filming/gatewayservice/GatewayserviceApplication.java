b0f992c517dc99400624f59331748fbc7622be19
dongyeop <hava12@naver.com>
2022-01-08 22:07:28 +0900

    GatewayService DiscoveryService 및 UserService 연동

--- src/main/java/com/filming/gatewayservice/GatewayserviceApplication.java
+++ src/main/java/com/filming/gatewayservice/GatewayserviceApplication.java
@@ -2,12 +2,13 @@ package com.filming.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayserviceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GatewayserviceApplication.class, args);
     }
-
 }
